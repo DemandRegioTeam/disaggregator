d557c687183e6ead506117f5cd990033a2b01d24
beierd <beier@uni-bremen.de>
2019-12-10 18:24:45 +0100

    Introduce setup.py to make repository installable

---
+++ setup.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+
+from setuptools import setup, find_packages
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='disaggregator',
+    version='0.0.1',
+    author='DemandRegioTeam',
+    author_email='fgotzens@fz-juelich.de',
+    description='A tool for disaggregating electrical and thermal demand in high resolution.',
+    namespace_package=['disaggregator'],
+    long_description=read('README.md'),
+    packages=find_packages(),
+    package_dir={'disaggregator': 'disaggregator'},
+ #   extras_require={
+ #         'dev': ['nose', 'sphinx', 'sphinx_rtd_theme', 'requests']},
+    install_requires=[
+        'pandas >= 0.17.0',
+        'PyYAML >= 5.1.1',
+        'requests >= 2.22.0',
+        'geopandas==0.4.1',
+        'xarray == 0.14.1',
+        'matplotlib >= 3.1.2'
+    ])
